68582c32cee7494b71b3d089a2d8a702cc4f010a
黒岩　菜海 <kuroiwanami@kuroiwasaikai-no-MacBook-Air.local>
2022-10-28 09:57:59 +0900

    開発環境不明点

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'lessons/hello', to: 'lessons#hello'
+  get 'lessons/call', to: 'lessons#call'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
